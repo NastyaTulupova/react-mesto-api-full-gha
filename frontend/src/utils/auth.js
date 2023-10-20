7f69e31ce67bb0532150835e103e14c9c920b253
Anastasia Tulupova <anast.tulupova@yandex.ru>
2023-10-20 14:01:23 +0300

    remote base url

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -1,5 +1,5 @@
-//export const BASE_URL = "https://api.mesto.tulupova.nomoredomainsrocks.ru";
-export const BASE_URL = "http://localhost:3000";
+export const BASE_URL = "https://api.mesto.tulupova.nomoredomainsrocks.ru";
+//export const BASE_URL = "http://localhost:3000";
 function checkResponse(res) {
   if (res.ok) {
     return res.json();
